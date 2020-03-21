73f31dda2337ec78d21964def039e8074469625c
Jamie Yoon <rkawk3316@gmail.com>
2020-03-21 21:13:43 +0200

    Added server2 using pug template

---
+++ week1/server-two/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const pug = require('pug');
+
+app.set('view engine', 'pug');
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+  res.render('index',{
+    title : 'Title',
+    header: 'Click on the cat',
+    name : 'Placeholder name'
+  })
+})
+app.get("/catinfo", (req, res) => {
+  const cat = {
+    name: "TestCat",
+    age: 2,
+    weight: 40
+  };
+  res.json(cat);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
