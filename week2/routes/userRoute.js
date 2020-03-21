881c230071a9c0e449c18f49a853dc0eeb5497bb
Jamie Yoon <rkawk3316@gmail.com>
2020-03-21 23:23:02 +0200

    Added post body log

--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -1,9 +1,17 @@
 "use strict";
 // userRoute
 const express = require("express");
+const app = express();
 const router = express.Router();
 const userController = require('../controllers/userController');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
+
 router.get('/', userController.user_list_get);
 router.get(`/:id`, userController.user_get);
-
+router.post('/', (req, res) => {
+    console.log(req.body);
+})
 module.exports = router;
