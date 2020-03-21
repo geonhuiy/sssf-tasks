558f0dd1f0667a873fdf696ffddfaaf0b9906ec3
Jamie Yoon <rkawk3316@gmail.com>
2020-03-21 20:03:27 +0200

    Added static directory

--- week1/server-one/index.js
+++ week1/server-one/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use('/static', express.static('public'))
 app.get("/", (req, res) => res.send("Hello world"));
-app.get('/catinfo', (req, res) => {
-    const cat = {
-        'name' : 'Cat1',
-        'age' : 1,
-        'weight' : 60 
-    };
-    res.json(cat);
-})
-app.use(express.static('public'));
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.get("/catinfo", (req, res) => {
+  const cat = {
+    name: "Cat1",
+    age: 1,
+    weight: 60
+  };
+  res.json(cat);
+});
 
+app.listen(port, () => console.log(`App listening on port ${port}`));
